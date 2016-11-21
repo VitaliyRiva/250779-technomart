f714468ba2645005b5690638bf99a8b50682dd71
VitaliyRiva <19a33abcd4>
2016-11-21 23:50:26 +0300

    Внёс правки

--- js/catalog-script.js
+++ js/catalog-script.js
@@ -8,30 +8,30 @@
 	// Цикличность действия вызова окна у товара на "Купить"
 	for (var i = 0; i < catalogbuy.length; i++) {
 		catalogbuy[i].addEventListener("click", function(event) {
-		event.preventDefault();
-	catalogbuypopup.classList.add("modal-content-show");
-	});
+			event.preventDefault();
+			catalogbuypopup.classList.add("modal-content-show");
+		});
 	}
 	// Закрытие модального окна на кнопку "Оформить заказ"
-		catalogbuychekout.addEventListener("click", function(event) {
+	catalogbuychekout.addEventListener("click", function(event) {
 		event.preventDefault();
 		catalogbuypopup.classList.remove("modal-content-show");
 	});
 		// Закрытие модального окна на кнопку "Продолжить покупки"
-		catalogbuycontshopp.addEventListener("click", function(event) {
+	catalogbuycontshopp.addEventListener("click", function(event) {
 		event.preventDefault();
 		catalogbuypopup.classList.remove("modal-content-show");
 	});
 		// Закрытие модального окна на кнопку "Крестик"
-		catalogbuyclose.addEventListener("click", function(event) {
+	catalogbuyclose.addEventListener("click", function(event) {
 		event.preventDefault();
 		catalogbuypopup.classList.remove("modal-content-show");
 	});
 		// Закрытие на клавишу "ESC"
-		window.addEventListener("keydown", function(event) {
+	window.addEventListener("keydown", function(event) {
 		if (event.keyCode === 27) {
 			if (catalogbuypopup.classList.contains("modal-content-show")) {
-			catalogbuypopup.classList.remove("modal-content-show");
+				catalogbuypopup.classList.remove("modal-content-show");
 			}
 		}
 	});
--- js/script.js
+++ js/script.js
@@ -8,21 +8,22 @@
 	var form = popup.querySelector("form");
 	var login = popup.querySelector("[name=login]");
 	var password = popup.querySelector("[name=password]");
-	var storage = localStorage.getItem("login");
-
+	if (window.localStorage) {
+	var storageName = localStorage.getItem("login");
+}
 	// Открытие модального окна "Форма входа"
 	link.addEventListener("click", function(event) {
 		event.preventDefault();
 		popup.classList.add("modal-content-show");
 
 	// Хранилище
-	if (storage) {
-	login.value = storage;
-	password.focus();
-	} else {
-	login.focus();
-	}
-});
+		if (storageName) {
+			login.value = storageName;
+			password.focus();
+		} else {
+			login.focus();
+		}
+	});
 	// Закрытие модального окна на кнопку "Крестик"
 	close.addEventListener("click", function(event) {
 		event.preventDefault();
@@ -33,57 +34,20 @@
 	//Отлов действия в форме на заполняемость Логина и Пароля
 	form.addEventListener("submit", function(event) {
 		if (!login.value || !password.value) {
-		event.preventDefault();
-		popup.classList.remove("modal-error");
-		popup.offsetWidth = popup.offsetWidth;
-		popup.classList.add("modal-error");
+			event.preventDefault();
+			popup.classList.remove("modal-error");
+			popup.offsetWidth = popup.offsetWidth;
+			popup.classList.add("modal-error");
 		} else {
-		localStorage.setItem("login", login.value);
+			localStorage.setItem("login", login.value);
 		}
 	});
 	// Закрытие на клавишу "ESC"
-		window.addEventListener("keydown", function(event) {
+	window.addEventListener("keydown", function(event) {
 		if (event.keyCode === 27) {
 			if (popup.classList.contains("modal-content-show")) {
-			popup.classList.remove("modal-content-show");
-			popup.classList.remove("modal-error");
-			}
-		}
-	});
-	// Работа с модальным окном при клике у товара на "Купить"
-	var catalogbuy = document.querySelectorAll(".buy");
-	var catalogbuypopup = document.querySelector(".modal-content-cart");
-	var catalogbuyclose = catalogbuypopup.querySelector(".modal-content-close");
-	var catalogbuychekout = catalogbuypopup.querySelector(".checkout");
-	var catalogbuycontshopp = catalogbuypopup.querySelector(".contshopp");
-
-	// Цикличность действия вызова окна у товара на "Купить"
-	for (var i = 0; i < catalogbuy.length; i++) {
-		catalogbuy[i].addEventListener("click", function(event) {
-		event.preventDefault();
-	catalogbuypopup.classList.add("modal-content-show");
-	});
-	}
-	// Закрытие модального окна на кнопку "Оформить заказ"
-		catalogbuychekout.addEventListener("click", function(event) {
-		event.preventDefault();
-		catalogbuypopup.classList.remove("modal-content-show");
-	});
-		// Закрытие модального окна на кнопку "Продолжить покупки"
-		catalogbuycontshopp.addEventListener("click", function(event) {
-		event.preventDefault();
-		catalogbuypopup.classList.remove("modal-content-show");
-	});
-		// Закрытие модального окна на кнопку "Крестик"
-		catalogbuyclose.addEventListener("click", function(event) {
-		event.preventDefault();
-		catalogbuypopup.classList.remove("modal-content-show");
-	});
-		// Закрытие на клавишу "ESC"
-		window.addEventListener("keydown", function(event) {
-		if (event.keyCode === 27) {
-			if (catalogbuypopup.classList.contains("modal-content-show")) {
-			catalogbuypopup.classList.remove("modal-content-show");
+				popup.classList.remove("modal-content-show");
+				popup.classList.remove("modal-error");
 			}
 		}
 	});
@@ -94,10 +58,11 @@
 	var feedbackformsend = feedbackformpopup.querySelector("form");
 	var feedbackformlogin = feedbackformpopup.querySelector("[name=user-name]");
 	var feedbackformemail = feedbackformpopup.querySelector("[name=user-email]");
-	var storage = localStorage.getItem("login-name");
-
+	if (window.localStorage) {
+	var storageLogin = localStorage.getItem("login-name");
+}
 		// Открытие модального окна  "Обратная связь" и фокуса на поле Имя
-		feedbackform.addEventListener("click", function(event) {
+	feedbackform.addEventListener("click", function(event) {
 		event.preventDefault();
 		feedbackformpopup.classList.add("modal-content-show");
 		feedbackformlogin.focus();
@@ -106,23 +71,23 @@
 	feedbackform.addEventListener("click", function(event) {
 		event.preventDefault();
 		feedbackformpopup.classList.add("modal-content-show");
-		if (storage) {
-		feedbackformlogin.value = storage;
-		feedbackformemail.value = storage;
-		feedbackformemail.focus();
+		if (storageLogin) {
+			feedbackformlogin.value = storageLogin;
+			feedbackformemail.value = storageLogin;
+			feedbackformemail.focus();
 		} else {
-		feedbackformlogin.focus();
+			feedbackformlogin.focus();
 		}
 	});
 	//Отлов действия в форме на заполняемость "Имени и Емэйла"
-		feedbackformsend.addEventListener("submit", function(event) {
-			if (!feedbackformlogin.value || !feedbackformemail.value) {
+	feedbackformsend.addEventListener("submit", function(event) {
+		if (!feedbackformlogin.value || !feedbackformemail.value) {
 			event.preventDefault();
 			feedbackformpopup.classList.remove("modal-error");
 			feedbackformpopup.offsetWidth = feedbackformpopup.offsetWidth;
 			feedbackformpopup.classList.add("modal-error");
 		} else {
-		localStorage.setItem("login-name", feedbackformlogin.value);
+			localStorage.setItem("login-name", feedbackformlogin.value);
 		}
 	});
 		// Закрытие модального окна
@@ -132,11 +97,11 @@
 		feedbackformpopup.classList.remove("modal-error");
 	});
 	// Закрытие на клавишу ESC
-		window.addEventListener("keydown", function(event) {
+	window.addEventListener("keydown", function(event) {
 		if (event.keyCode === 27) {
 			if (feedbackformpopup.classList.contains("modal-content-show")) {
-			feedbackformpopup.classList.remove("modal-content-show");
-			feedbackformpopup.classList.remove("modal-error");
+				feedbackformpopup.classList.remove("modal-content-show");
+				feedbackformpopup.classList.remove("modal-error");
 			}
 		}
 	});
@@ -156,10 +121,10 @@
 		mapPopup.classList.remove("modal-content-show");
 	});
 		// Закрытие на клавишу "ESC"
-		window.addEventListener("keydown", function(event) {
+	window.addEventListener("keydown", function(event) {
 		if (event.keyCode === 27) {
 			if (mapPopup.classList.contains("modal-content-show")) {
-			mapPopup.classList.remove("modal-content-show");
+				mapPopup.classList.remove("modal-content-show");
 			}
 		}
 	});
